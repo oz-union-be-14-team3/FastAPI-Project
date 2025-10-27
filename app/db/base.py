8bfbc12f9417566bfb0b12471d1078d468a681b8
choiguen <daindj0@gmail.com>
2025-10-27 18:53:06 +0900

    feat: Tortoise ORM DB 연결 초기화 함수 추가

---
+++ app/db/base.py
@@ -0,0 +1,11 @@
+from tortoise.contrib.fastapi import register_tortoise
+from app.core.config import settings
+
+def db_connection(app):
+    register_tortoise(
+        app,
+        db_url=settings.DB_URL,
+        modules={"models", ["app.models"]},
+        generate_schemas=False,
+        add_exception_handlers=True
+    )
