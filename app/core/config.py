b49f8b9ceeb20eb26da6c2a77b2ec8b732fd1ddf
choiguen <daindj0@gmail.com>
2025-10-27 18:21:12 +0900

    chore: 환경변수 파일 수정
    - config.py파일에 DB_URL 추가>

--- app/core/config.py
+++ app/core/config.py
@@ -12,6 +12,10 @@ class Settings(BaseSettings):
     DB_PORT: str
     DB_NAME: str
 
+    @property
+    def DB_URL(self):
+        return f"{self.DB_SCHEME}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     model_config = SettingsConfigDict(
         env_file=str(".env"),
         extra="ignore"
