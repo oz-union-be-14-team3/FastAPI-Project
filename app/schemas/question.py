f92ddeb165b701f395f9ac6e9a9ef9092647fbe2
choiguen <daindj0@gmail.com>
2025-10-30 13:24:21 +0900

    feat: 몰루

--- app/schemas/question.py
+++ app/schemas/question.py
@@ -1,6 +1,25 @@
 from pydantic import BaseModel
+from datetime import datetime
 
-
-class Test(BaseModel):
-    id : int
+class QuestionBase(BaseModel):
     question_text: str
+
+class QuestionResponse(QuestionBase):
+    id: int
+
+    class Config:
+        # Pydantic이 ORM 객체(Tortoise Model)에서 데이터를 읽을 수 있게 합니다.
+        from_attributes = True
+
+class UserQuestionBase(BaseModel):
+    pass # 추가 필드 없음
+
+class UserQuestionResponse(UserQuestionBase):
+    id: int
+    user_id: int
+    quote_id: int
+    created_at: datetime
+
+    class Config:
+        # Tortoise ORM 객체와 Pydantic 호환을 위한 설정
+        from_attributes = True
